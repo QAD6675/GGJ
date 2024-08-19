406b89ca4f4a4437b4cf4a7ef6eb04115e237174
captain-sc <74822612+captain-sc@users.noreply.github.com>
2024-08-19 15:33:10 +0530

    added basic dog ai

--- Assets/Code/Dog.cs
+++ Assets/Code/Dog.cs
@@ -1,23 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+
 
 public class Dog : MonoBehaviour
 {
-    public Transform player;
-    // private NavMeshAgent navMeshAgent;
+    public Transform catTransform; 
+    public float sightRange = 10f; 
+    public float chaseSpeed = 5f;  
+    public float wanderSpeed = 2f; 
+    public float wanderRadius = 5f; 
+    public float attackRange = 1.5f; 
 
-    private void Start()
+    private Vector3 wanderTarget; 
+    private bool isChasing = false;
+
+    void Start()
     {
-        // navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Wander());
     }
 
-    private void Update()
+    void Update()
     {
-        if (player != null)
+        float distanceToCat = Vector3.Distance(transform.position, catTransform.position);
+
+        if (distanceToCat <= sightRange)
         {
-            // navMeshAgent.SetDestination(player.position);
+            isChasing = true;
+            ChaseCat();
+        }
+        else
+        {
+            isChasing = false;
         }
     }
+
+    private void ChaseCat()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, catTransform.position, chaseSpeed * Time.deltaTime);
+
+        
+        if (catTransform.position.x > transform.position.x)
+            transform.localScale = new Vector3(1, 1, 1); 
+        else
+            transform.localScale = new Vector3(-1, 1, 1); 
+
+        if (Vector3.Distance(transform.position, catTransform.position) <= attackRange)
+        {
+            AttackCat();
+        }
+    }
+
+    private void AttackCat()
+    {
+        //here comes the attack anim
+        Debug.Log("Dog is attacking the cat!");
+    }
+
+    private IEnumerator Wander()
+    {
+        while (!isChasing)
+        {
+            wanderTarget = new Vector3(transform.position.x + Random.Range(-wanderRadius, wanderRadius), transform.position.y, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, wanderTarget, wanderSpeed * Time.deltaTime);
+
+         
+            if (wanderTarget.x > transform.position.x)
+                transform.localScale = new Vector3(1, 1, 1);
+            else
+                transform.localScale = new Vector3(-1, 1, 1); 
+
+            yield return new WaitForSeconds(Random.Range(2f, 5f));
+        }
+    }
+
 }
